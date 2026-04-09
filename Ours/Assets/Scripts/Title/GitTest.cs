34a714dbede2d1f76120f40942badf48c73cbed1
DAVID <cjm143@naver.com>
2026-04-09 10:53:13 +0900

    테스트 : 깃 연결 확인 로그 추가

---
+++ Ours/Assets/Scripts/Title/GitTest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("데스크탑으로 깃 연결 성공");
+    }
+
+
+}
